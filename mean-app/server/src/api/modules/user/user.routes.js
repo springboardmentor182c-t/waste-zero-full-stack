0798441c45158a26b63e3f8ca4e549bd8acad9be
Abimanyu24-Tn <abimanyu24112005@gmail.com>
2025-09-18 22:37:56 +0530

    profile api added

--- mean-app/server/src/api/modules/user/user.routes.js
+++ mean-app/server/src/api/modules/user/user.routes.js
@@ -7,4 +7,11 @@ router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 
+// User Profile APIs
+router.get("/users/:id", userController.getUserById); 
+router.get("/users", userController.getAllUsers);   
+router.post("/users", userController.createUser);      
+router.put("/users/:id", userController.updateUser);  
+router.delete("/users/:id", userController.deleteUser); 
+
 export default router;
