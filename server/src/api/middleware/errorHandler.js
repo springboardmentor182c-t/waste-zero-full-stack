2ae4b5ebd2ad25216a15b97ace0bdeac4bb53fa2
springboardmentor182c-t <springboardmentor182c@gmail.com>
2025-09-19 16:55:39 +0530

    Delete server directory

--- server/src/api/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-export default function errorHandler(err, req, res, next) {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    error: {
-      code: err.status || 500,
-      message: err.message || "Internal Server Error"
-    }
-  });
-}
