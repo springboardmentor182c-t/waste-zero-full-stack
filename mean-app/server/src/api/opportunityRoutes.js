6c5701168a476371377403eb8d6da76aa3d2fcd3
vijay patidar <132213385+Vijay043@users.noreply.github.com>
2025-10-10 18:53:20 +0530

    Delete mean-app/server/src/api/opportunityRoutes.js

--- mean-app/server/src/api/opportunityRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createOpportunity, getOpportunities } = require("../controllers/opportunityController");
-
-const router = express.Router();
-
-router.post("/", createOpportunity);
-router.get("/", getOpportunities);
-
-module.exports = router;
