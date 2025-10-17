6b6b179cf421a20b87e5d65d095ebcc71c3128e3
sakshiadmuthe13 <sakshiadmuthe13@gmail.com>
2025-10-17 12:10:45 +0530

    Delete mean-app/server/src/api/modules/message/message.routes.ts

--- mean-app/server/src/api/modules/message/message.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendMessage, getMessages } from "./message.controller";
-
-const router = express.Router();
-
-router.post("/", sendMessage);
-router.get("/:senderId/:receiverId", getMessages);
-
-export default router;
