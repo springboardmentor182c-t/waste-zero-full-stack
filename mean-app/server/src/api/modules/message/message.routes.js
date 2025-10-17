c5d0b1f47a46d88529e63840ecaf62016b3aa84f
sakshiadmuthe13 <sakshiadmuthe13@gmail.com>
2025-10-17 12:16:29 +0530

    add message.route.js

---
+++ mean-app/server/src/api/modules/message/message.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { sendMessage, getMessages } from "./message.controller";
+
+const router = express.Router();
+
+router.post("/", sendMessage);
+router.get("/:senderId/:receiverId", getMessages);
+router.get("/:senderId/:receiverId", getMessages);
+
+export default router;
