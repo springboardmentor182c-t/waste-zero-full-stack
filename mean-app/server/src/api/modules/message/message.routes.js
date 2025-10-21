7edf3cb7e0acbd8d165868f859c0e8b4f72f6bed
sakshiadmuthe13 <sakshiadmuthe13@gmail.com>
2025-10-21 23:11:49 +0530

    update message.routes.js file

--- mean-app/server/src/api/modules/message/message.routes.js
+++ mean-app/server/src/api/modules/message/message.routes.js
@@ -1,10 +1,20 @@
-import express from "express";
-import { sendMessage, getMessages } from "./message.controller";
-
+const express = require("express");
 const router = express.Router();
+const messagesController = require("./message.controller");
 
-router.post("/", sendMessage);
-router.get("/:senderId/:receiverId", getMessages);
-router.get("/:senderId/:receiverId", getMessages);
+// Send a message
+router.post("/send", messagesController.sendMessage);
 
-export default router;
+// Get conversation between two users
+router.get("/conversation/:user1_id/:user2_id", messagesController.getConversation);
+
+// Get all conversations for a user
+router.get("/conversations/:user_id", messagesController.getUserConversations);
+
+// Mark messages as read
+router.put("/mark-read", messagesController.markAsRead);
+
+// Delete a message
+router.delete("/:messageId", messagesController.deleteMessage);
+
+module.exports = router;
