15fd00bf1a65c5b40a3530abaf5978837da34b5a
Gayathri <231801360008@cutmap.ac.in>
2025-10-21 18:45:29 +0530

    Add files via upload

---
+++ mean-app/client/src/Messages UI/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MessagesPage from './MessagesPage'
+
+function App() {
+  return (
+    <div className="App">
+      <MessagesPage />
+    </div>
+  )
+}
+
+export default App
