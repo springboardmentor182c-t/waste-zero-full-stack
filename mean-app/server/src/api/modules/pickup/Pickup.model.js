8f5761bda9182b6d8b92bea0a45c9f18deac36ac
Abimanyu24-Tn <abimanyu24112005@gmail.com>
2025-10-23 20:11:15 +0530

    pickup-api modified

--- mean-app/server/src/api/modules/pickup/Pickup.model.js
+++ mean-app/server/src/api/modules/pickup/Pickup.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const pickupSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -9,4 +9,6 @@ const pickupSchema = new mongoose.Schema({
     status: { type: String, default: 'Scheduled' } // Scheduled / Completed / Cancelled
 }, { timestamps: true });
 
-module.exports = mongoose.model('Pickup', pickupSchema);
+const Pickup = mongoose.models.Pickup || mongoose.model("Pickup", pickupSchema);
+
+export default Pickup;
