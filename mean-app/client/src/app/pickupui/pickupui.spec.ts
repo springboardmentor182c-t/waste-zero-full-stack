32af2f674093429d447de88bbd8e01de9761377d
satyasai <kadavergusatyasai0719@gmail.com>
2025-10-24 18:08:56 +0530

    a

--- mean-app/client/src/app/pickupui/pickupui.spec.ts
+++ mean-app/client/src/app/pickupui/pickupui.spec.ts
@@ -14,7 +14,7 @@ describe('Pickupui', () => {
 
     fixture = TestBed.createComponent(Pickupui);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges();   
   });
 
   it('should create', () => {
